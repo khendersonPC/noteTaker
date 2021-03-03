7ff5fe071168ab423e9b945d1f30e3823dc36d99
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-03-02 17:39:44 -0700

    Working

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-app.use(express.static('public'));
+
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 
@@ -8,8 +8,9 @@ const express = require('express');
 // This sets up the basic properties for our express server
 
 // Tells node that we are creating an "express" server
-const app = express();
 
+const app = express();
+app.use(express.static('public'));
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
 
